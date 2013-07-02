49861fff530db2ddb755998b9890d942c69ae2c5
sanemat <o.gata.ken@gmail.com>
2013-07-03 03:27:48 +0900

    Test Tachikoma::Settings

---
+++ spec/tachikoma/settings_spec.rb
@@ -0,0 +1,16 @@
+require 'tachikoma/settings'
+describe Tachikoma do
+  describe '.root_path' do
+    let(:somewhere) { '/path/to/somewhere' }
+    let(:datapath) { somewhere + '/data'}
+    let(:repospath) { somewhere + '/repos'}
+
+    before :each do
+      Dir.stub(:pwd).and_return(somewhere)
+    end
+
+    it { expect(described_class.root_path).to eq Pathname.new(somewhere) }
+    it { expect(described_class.data_path).to eq Pathname.new(datapath) }
+    it { expect(described_class.repos_path).to eq Pathname.new(repospath) }
+  end
+end
