23e6ae3efb0cd2bba3f5a3b6b1727901f52e1054
sanemat <o.gata.ken@gmail.com>
2013-11-24 13:39:32 +0900

    Force new syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,7 @@
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
+  config.raise_errors_for_deprecations!
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
