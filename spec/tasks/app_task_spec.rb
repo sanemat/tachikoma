08cd81492fc9a90300c404b9b5519b2a0eda5115
Kohei Hasegawa <ameutau@gmail.com>
2013-12-18 22:24:28 +0900

    Replace stub_chain

--- spec/tasks/app_task_spec.rb
+++ spec/tasks/app_task_spec.rb
@@ -6,13 +6,7 @@ describe 'app.rake' do
   describe 'tachikoma:pull_request' do
     context 'when github returns 422 UnprocessableEntity' do
       before do
-        # FIXME: Using `stub_chain` from rspec-mocks' old `:should`
-        #        syntax without explicitly enabling the syntax is deprecated.
-        #        Use the new `:expect` syntax or explicitly enable `:should`
-        #        instead. Called from
-        #        /Users/sane/work/ruby-study/tachikoma/spec/tasks/app_task_spec.rb:9:
-        #        in `block (4 levels) in <top (required)>'.
-        Octokit::Client.stub_chain(:new, :create_pull_request).and_raise(Octokit::UnprocessableEntity)
+        allow(Octokit::Client).to receive_message_chain(:new, :create_pull_request).and_raise(Octokit::UnprocessableEntity)
       end
 
       it 'should not raise Octokit::UnprocessableEntity error' do
