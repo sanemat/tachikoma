3e2683d67db6dbf71ac83458581db99a8a7fe294
sanemat <o.gata.ken@gmail.com>
2014-09-28 05:29:45 +0900

    Strategy: cocoapods

--- lib/tachikoma/application.rb
+++ lib/tachikoma/application.rb
@@ -139,6 +139,19 @@ module Tachikoma
       end
     end
 
+    def cocoapods
+      Dir.chdir("#{Tachikoma.repos_path}/#{@build_for}") do
+        sh "git config user.name #{@commiter_name}"
+        sh "git config user.email #{@commiter_email}"
+        sh "git checkout -b tachikoma/update-#{@readable_time} #{@base_remote_branch}"
+        sh 'pods install'
+        sh 'pods update'
+        sh 'git add Podfile.lock'
+        sh %Q(git commit -m "Cocoapods update #{@readable_time}") do; end # ignore exitstatus
+        sh "git push #{@authorized_compare_url} tachikoma/update-#{@readable_time}"
+      end
+    end
+
     def pull_request
       @client = Octokit::Client.new access_token: @github_token
       @client.create_pull_request(@pull_request_url, @pull_request_base, @pull_request_head, @pull_request_title, @pull_request_body)
